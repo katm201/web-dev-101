59fb29fd2cfb3d0b2cc5001fd6ee8fce63ab9acd
Katherine Manning <katherine.manning@ask.com>
2019-01-16 17:05:52 -0800

    [6] Add txt file to save strings

--- server/storage.js
+++ server/storage.js
@@ -1,11 +1,15 @@
-const strings = [];
+const fs = require('fs');
+const path = require('path');
 
 const addString = function(string) {
-    strings.push(string);
+    const text = fs.readFileSync(path.join(__dirname, 'strings.txt'), 'utf8');
+    fs.writeFileSync(path.join(__dirname, 'strings.txt'), text + '\n' + string, 'utf8');
     return { statusCode: 201, data: 'ok' };
 }
 
 const getStrings = function() {
+    const text = fs.readFileSync(path.join(__dirname, 'strings.txt'), 'utf8');
+    const strings = text.split('\n');
     return { data: JSON.stringify(strings) };
 }
 
