ac512451d0c4d184ddbc3859c74b7b00ded79854
Katherine Manning <katherine.manning@ask.com>
2019-01-18 16:40:38 -0800

    [9] Add query string for parsing

--- server/storage.js
+++ server/storage.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const queryString = require('query-string');
 
 const addString = function(string) {
+    const formattedObj = queryString.parse(string);
+    const formatted = Object.keys(formattedObj)[0];
     const text = fs.readFileSync(path.join(__dirname, 'strings.txt'), 'utf8');
-    fs.writeFileSync(path.join(__dirname, 'strings.txt'), text + '\n' + string, 'utf8');
+    fs.writeFileSync(path.join(__dirname, 'strings.txt'), text + '\n' + formatted, 'utf8');
     return { statusCode: 201, data: 'ok' };
 }
 
