8ef7cb5a0e26ed404823826fe20481fd7f4df60f
Katherine Manning <katherine.manning@ask.com>
2019-01-18 16:56:16 -0800

    [10] Add express & set up static assets

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
 const router = require('./router');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+app.use('/', router);
 
 const port = 3000;
 const ip = '127.0.0.1';
 
-const server = http.createServer(router);
+app.listen(port);
 console.log(`Listening on http://${ip}:${port}`);
-server.listen(port, ip);
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const path = require('path');
 const url = require('url');
 
 const storage = require('./storage');
@@ -8,33 +6,10 @@ const serveError = function() {
     return { statusCode: 404, data: 'Not Found' };
 }
 
-const sendOptions = function() {
-    return {};
-}
-
-const sendFiles = function(route) {
-    const [, directory, filename] = route.split('/');
-    const filetype = filename.split('.')[1];
-    const type = filetype === 'js' ? 'javascript' : filetype;
-    const filepath = path.join(__dirname, '..', 'client', directory, filename);
-    const data = fs.readFileSync(filepath, 'utf8');
-    return { data, contentType: `text/${type}` };
-}
-
 const routes = function(route, method) {
     const { pathname, query } = url.parse(route);
     const baseRoute = '/' + pathname.substring(1).split('/')[0];
     const routeDictionary = {
-        '/': {
-            'GET': () => sendFiles('/./index.html'),
-            'OPTIONS': sendOptions,
-        },
-        '/assets': {
-            'GET': sendFiles,
-        },
-        '/scripts': {
-            'GET': sendFiles,
-        },
         '/storage': {
             'GET': () => storage.getStrings(),
             'POST': () => storage.addString(query),
